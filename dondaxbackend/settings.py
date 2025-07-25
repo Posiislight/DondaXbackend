fb0b4cae69de4c007adc09254da57241774111f5
Posiislight <adelekeolamiposi@gmail.com>
2025-07-25 18:22:44 +0100

    changed email host user

--- dondaxbackend/settings.py
+++ dondaxbackend/settings.py
@@ -137,7 +137,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'adelekeolamiposi@gmail.com'  # Brevo login email
+EMAIL_HOST_USER = 'care@dondaxlimited.com'  # Brevo login email
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")        # Brevo SMTP key
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
